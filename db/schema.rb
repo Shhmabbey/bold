f138f9885a85febd98e3c48596bba559c16fa7a5
shhmabbey <abhernndz@gmail.com>
2022-03-20 19:32:28 -0700

    Setup Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_020317) do
+ActiveRecord::Schema.define(version: 2022_03_21_022157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 2022_03_21_020317) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_categories_on_category_id"
+    t.index ["title"], name: "index_categories_on_title", unique: true
   end
 
   create_table "products", force: :cascade do |t|
