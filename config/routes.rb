75ee01b656f0167bb6d9e4618c149eccaacb90e4
shhmabbey <abhernndz@gmail.com>
2022-03-21 10:31:02 -0700

    Route carts

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy]
-    resources :users, only: [:create, :show]
+    resources :users, only: [:create, :show] do
+      resources :carts, only: [:show, :index, :create]
+    end
     resources :products, only: [:index, :show]
+    resources :carts, only: [:show, :index] do 
+      # resources :cart_products, only: [:index, :show, :create, :update, :destroy]
+    end
   end
 
 end
