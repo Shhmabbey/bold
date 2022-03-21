cc5ca1cde8c3d38c92e76bd6887e3f6a084a212a
shhmabbey <abhernndz@gmail.com>
2022-03-20 20:11:04 -0700

    Provide create and show users actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy]
+    resources :users, only: [:create, :show]
   end
-
 
 end
