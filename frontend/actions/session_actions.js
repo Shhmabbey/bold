6b33703cb1d4e911bc3a62d990c55dfad93a88aa
shhmabbey <abhernndz@gmail.com>
2022-03-22 11:41:28 -0700

    Debug reducers and controllers

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -26,13 +26,13 @@ export const clearSessionErrors = () => {
   }
 };
 
-export const signup = user => dispatch => (
-  APIUtil.signup(user).then(user => (
+export const signup = user => dispatch => {
+  return APIUtil.signup(user).then(user => (
     dispatch(receiveCurrentUser(user))
   ), err => (
     dispatch(receiveErrors(err.responseJSON))
   ))
-);
+}
 
 export const login = user => dispatch => (
   APIUtil.login(user).then(user => (
